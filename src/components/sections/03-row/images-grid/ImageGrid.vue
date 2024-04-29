f492b704a28824a36dc55c10b2d38c4ea7f8d596
alejandro.cano@imagineapps.co <alejandroyunes@outlook.com>
2024-04-29 06:24:22 -0500

    fixed colors

--- src/components/sections/03-row/images-grid/ImageGrid.vue
+++ src/components/sections/03-row/images-grid/ImageGrid.vue
@@ -8,22 +8,23 @@ import './image-grid.scss'
 
     <div class="left-side">
       <div>
-        <img class="first-image" src="/images/sections/03-row/grid-image/close-mic.webp" alt="" width={0} height={0} />
+        <img class="first-image" src="/images/sections/03-row/grid-image/close-mic.webp" alt="" />
       </div>
 
       <div class="second-image">
         <div>
-          <img class="image" src="/images/sections/03-row/grid-image/grid1.webp" alt="" width={0} height={0} />
+          <img class="image" src="/images/sections/03-row/grid-image/grid1.webp" alt="" />
         </div>
         <div>
-          <img class="image" src="/images/sections/03-row/grid-image/grid2.webp" alt="" width={0} height={0} />
+          <img class="image" src="/images/sections/03-row/grid-image/grid2.webp" alt="" />
         </div>
       </div>
 
     </div>
 
     <div>
-      <img class="billboard" src="/images/billboard/musicP.webp" alt="" width={0} height={0} />
+      <img class="billboard" src="/images/billboard/musicP.webp" alt="" />
     </div>
+
   </section>
 </template>
